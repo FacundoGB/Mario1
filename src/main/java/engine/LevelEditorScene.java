60f7046ff8377b7b4e77f53d5050576f0a787817
FacundoGB <bardifacundo@gmail.com>
2022-05-14 17:53:26 -0300

    Removen proof of concept delta time in LES

--- src/main/java/engine/LevelEditorScene.java
+++ src/main/java/engine/LevelEditorScene.java
@@ -1,33 +1,13 @@
 package engine;
 
-import java.awt.event.KeyEvent;
 
 public class LevelEditorScene extends Scene{
 
-    private boolean ChangingScene = false;
-    private float timeToChangeScene = 2.0f;
     public LevelEditorScene(){
-        System.out.println("Inside Level Editor Scene");
-
     }
 
     @Override
     public void update(float dt) {
-        System.out.println("Running at: " + (1.0f / dt) + "FPS");
-
-        if (!ChangingScene && KeyListener.isKeyPressed(KeyEvent.VK_SPACE)){
-            ChangingScene = true;
-        }
-
-        if (ChangingScene && timeToChangeScene > 0){
-            timeToChangeScene -= dt;
-            Window.get().r -= dt * 5.0f;
-            Window.get().g -= dt * 5.0f;
-            Window.get().b -= dt * 5.0f;
-        }
-        else if (ChangingScene){
-            Window.changeScene(1);
-        }
     }
 
 
